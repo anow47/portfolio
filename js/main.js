15e7ae772a121373194122eb7423e07530364888
anow47 <anow47@yahoo.com>
2021-12-24 17:08:16 +0200

    added all project files

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//////////////////////////////////////////// dark mode function
+function lightMode() {
+  let element = document.body;
+  element.classList.toggle("light-mode");
+}
+//Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
+function toggleMenu() {
+  let showMenu = document.getElementById("ul");
+  if (showMenu.style.display === "none") {
+    showMenu.style.display = "block";
+  } else {
+    showMenu.style.display = "none";
+  }
+}
+
+////////////////////////////// Animation on scroll element fade-in
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
