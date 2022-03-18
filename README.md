d5009acfaea3f2447bf0230b64d4786218022ac5
Dark <anow47@yahoo.com>
2022-03-18 19:19:10 +0200

    changed the work section

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # portfolio
-my personal portfolio
+my personal portfolio.
 I have designed and hard coded it from scratch, using only
 HTML, CSS and plain JavaScript.
 So feel free to use my code on your project or if you have any question or comment
--- work/Hamouda Mohammed2_files/main.js
+++ work/Hamouda Mohammed2_files/main.js
@@ -12,22 +12,6 @@ function toggleMenu() {
     showMenu.style.display = "none";
   }
 }
-//slide up a project to show more info
-function workInfo() {
-  const images = document.querySelector("#snapShot1");
-  let showInfo = document.querySelector("#info1");
-  if (images.style.height === "500px") {
-    images.style.height = "300px";
-    images.style.borderBottomLeftRadius = "0px";
-    images.style.borderBottomRightRadius = "0px";
-    showInfo.style.display = "block";
-    // console.log("info");
-  } else {
-    images.style.height = "500px";
-    images.style.borderRadius = "10px"
-    showInfo.style.display = "none";
-  }
-}
 //More about me
 function showMore() {
   let showText = document.getElementById("text");
@@ -38,6 +22,29 @@ function showMore() {
   }
 }
 
+//Work images full screen display
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.querySelectorAll("image1, image2, image3");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+////////////////////////////////////////////////////
 //Animation on scroll element fade-in
 
 const scrollElements = document.querySelectorAll(".js-scroll");
