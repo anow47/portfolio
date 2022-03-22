c8b6814601dc2105be97e7c7d059f0aacbeca99f
Dark <anow47@yahoo.com>
2022-03-22 19:47:12 +0200

    modified js file

--- js/main.js
+++ js/main.js
@@ -28,6 +28,36 @@ function workInfo() {
     showInfo.style.display = "none";
   }
 }
+function workInfoTwo() {
+  const images = document.querySelector("#snapShot2");
+  let showInfo = document.querySelector("#info2");
+  if (images.style.height === "500px") {
+    images.style.height = "300px";
+    images.style.borderBottomLeftRadius = "0px";
+    images.style.borderBottomRightRadius = "0px";
+    showInfo.style.display = "block";
+    // console.log("info");
+  } else {
+    images.style.height = "500px";
+    images.style.borderRadius = "10px"
+    showInfo.style.display = "none";
+  }
+}
+function workInfoThree() {
+  const images = document.querySelector("#snapShot3");
+  let showInfo = document.querySelector("#info3");
+  if (images.style.height === "500px") {
+    images.style.height = "300px";
+    images.style.borderBottomLeftRadius = "0px";
+    images.style.borderBottomRightRadius = "0px";
+    showInfo.style.display = "block";
+    // console.log("info");
+  } else {
+    images.style.height = "500px";
+    images.style.borderRadius = "10px"
+    showInfo.style.display = "none";
+  }
+}
 //More about me
 function showMore() {
   let showText = document.getElementById("text");
