042595f6a9dfd5deff376eaab547e5f2a5e1bdb3
unknown <anow47@yahoo.com>
2023-07-12 19:09:17 +0200

    restructured the website

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 //Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
 let mask = document.getElementById("mask")
 let showMenu = document.getElementById("mobile-ul");
+let logo = document.getElementById("logo-mobile");
 function toggleMenu() {
   mask.style.display === "none"? mask.style.display = "block" : mask.style.display = "none"
   showMenu.style.display === "none" ? showMenu.style.display = "block" : showMenu.style.display = "none";
+  logo.style.display = "none"
 }
 function closeToggleMenu() {
   showMenu.style.display = "none"
   mask.style.display = "none"
+  logo.style.display = "block"
 }
 
 function closeMenu() {
@@ -15,9 +18,21 @@ function closeMenu() {
   mask.style.display = "none"
 }
 
+// add active class to nav links
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach((link) => {
+  link.addEventListener('click', function() {
+    // remove active class from all navigation links
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+    });
+    // add active class to the clicked link
+    this.classList.add('active');
+  });
+});
+
 // change toggle icon with arrow function
 // changeIcon = (icon)=> icon.classList.toggle('fa-times')
-
 
 //More about me
 let showText = document.getElementById("text");
@@ -62,8 +77,11 @@ const handleScrollAnimation = () => {
       hideScrollElement(el);
     }
   });
+  
+  // Remove the scroll event listener if all elements have been displayed
+  if (document.querySelectorAll(".js-scroll.scrolled").length === scrollElements.length) {
+    window.removeEventListener("scroll", handleScrollAnimation);
+  }
 };
 
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
-});
+window.addEventListener("scroll", handleScrollAnimation);
